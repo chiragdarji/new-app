3a2ff41185ffee7b4d9af7db8a1082d12b71a148
Chirag Darji <chiragdarji.savli@gmail.com>
2017-06-03 17:49:55 +0530

    add new component for alert warning and error

---
+++ src/app/alert/error/error.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-alert-error',
+  templateUrl: 'error.component.html'
+})
+export class ErrorComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ErrorComponent} from './alert/error/error.component'
+import {WarningComponent} from './alert/warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule
