8e6aba7e80f6dc48dfb94d6819f313db2364d8ee
Chirag Darji <chiragdarji.savli@gmail.com>
2017-06-03 23:42:13 +0530

    Add new server to learn data binding and event binding

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  serverId = 10;
+  serverStatus = "offline";
+  allowAddNewServer = false;
+
+  constructor() {
+     setTimeout(() => {
+        this.allowAddNewServer = true;
+     }, 2000)
+  }
+
+  ngOnInit() {
+
+  }
+
+}
